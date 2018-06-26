da8fce4a9a2a99b0ebda7badd7dd7f1698d6227b
Justin Kenney <justinkenney13@gmail.com>
2018-06-26 23:38:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,8 +13,8 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
-    
-    
+
+
     end
 
   end
@@ -28,8 +28,8 @@ class Scraper
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
-      course.description = post.css("p").text   
-    end    
+      course.description = post.css("p").text
+    end
   end
 
   #   def print_courses
